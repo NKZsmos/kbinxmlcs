f8f86d2a3a52f739727f9010520a8a05f0d4e413
Milkitic <Milkitic@126.com>
2021-04-27 11:03:20 +0800

    add KbinEncodings enum

---
+++ kbinxmlcs/KbinEncodings.cs
@@ -0,0 +1,11 @@
+﻿namespace kbinxmlcs
+{
+    public enum KbinEncodings
+    {
+        ShiftJIS,
+        ASCII,
+        ISO_8859_1,
+        EUCJP,
+        UTF8
+    }
+}
---
+++ kbinxmlcs/KbinExtension.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace kbinxmlcs
+{
+    public static class KbinExtension
+    {
+        public static XElement ReadXElement(this KbinReader reader)
+        {
+            return reader.ReadLinq().Root;
+        }
+
+        public static KbinWriter FromKbinEncoding(XNode node, KbinEncodings enumencoding)
+        {
+            Encoding encoding = null;
+            switch (enumencoding)
+            {
+                case KbinEncodings.ShiftJIS:
+                    encoding = Encoding.GetEncoding("shift_jis");
+                    break;
+                case KbinEncodings.ASCII:
+                    encoding = Encoding.ASCII;
+                    break;
+                case KbinEncodings.ISO_8859_1:
+                    encoding = Encoding.GetEncoding("iso-8859-1");
+                    break;
+                case KbinEncodings.EUCJP:
+                    encoding = Encoding.GetEncoding("EUC-JP");
+                    break;
+                case KbinEncodings.UTF8:
+                    encoding = Encoding.UTF8;
+                    break;
+                default:
+                    break;
+            }
+
+            return new KbinWriter(node, encoding);
+        }
+    }
+}
